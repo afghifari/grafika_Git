76438bdd79e0ed381848312b89df030a4ef59d72
afghifari <alhudaghifari@gmail.com>
2017-01-29 00:07:58 +0700

    tugas1 done

---
+++ tugas1_movingteks/main.c
@@ -0,0 +1,108 @@
+#include "name.c"
+
+ int main()
+ {
+     int x = 0, y = 0;
+     int i;     
+     long int location = 0;
+
+     // Open the file for reading and writing
+     fbfd = open("/dev/fb0", O_RDWR);
+     if (fbfd == -1) {
+         perror("Error: cannot open framebuffer device");
+         exit(1);
+     }
+     printf("The framebuffer device was opened successfully.\n");
+
+     // Get fixed screen information
+     if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+         perror("Error reading fixed information");
+         exit(2);
+     }
+
+     // Get variable screen information
+     if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+         perror("Error reading variable information");
+         exit(3);
+     }
+
+     printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
+
+     // Figure out the size of the screen in bytes
+     screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+
+     // Map the device to memory
+     fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED,
+                        fbfd, 0);
+     if ((int)fbp == -1) {
+         perror("Error: failed to map framebuffer device to memory");
+         exit(4);
+     }
+     printf("The framebuffer device was mapped to memory successfully.\n");
+	displayWidth = vinfo.xres;
+	displayHeight = vinfo.yres;
+     x = 40; y = 50;       // Where we are going to put the pixel
+
+     //define color
+	//black
+	color X;
+	X.R = 0;
+	X.G = 0;
+	X.B = 0;
+
+    //yellow
+    color C;
+    C.R = 255;
+    C.G = 255;
+    C.B = 10;
+
+    //blue 
+    color B;
+    B.R = 66;
+    B.G = 134;
+    B.B = 244;
+
+     // Figure out where in memory to put the pixel
+     i = 750;
+     while (i > -750) {
+		kelompok7(400, i, C);
+		 nameFahri(400, 100+i, C);
+		 nameGumi(400, 200+i, C);
+		 nameJoshua(400, 300+i, C);
+		 nameMalvin(400, 400+i, C);
+		 nameRaihan(400, 500+i, C);
+		 nameRaja(400, 600+i, C);
+		 nameSeptialoka(400, 700+i, C);
+		 i -= 10;
+		usleep(50000);
+        
+        if (i%20 == 0) printBackground(X);
+
+	 }
+
+// 	printSquare(SIZE, 3*SIZE+23, 7*SIZE+133, B);
+// printA(50,100,C);	
+// printB(100,100,C);
+// printC(150,100,C);
+// printD(200,100,C);
+// printE(250,100,C);  
+// printF(300,100,C);
+// printG(350,100,C);
+// printH(400,100,C);
+// printI(450,100,C);
+// printJ(470,100,C);
+// printK(520,100,C);
+// printL(570,100,C);
+// printM(620,100,C);
+// printN(680,100,C);
+// printO(740,100,C);
+// printP(790,100,C);
+// printR(840,100,B);
+// printS(890,100,B);
+// printT(940,100,B);
+// printV(990,100,B);
+
+     munmap(fbp, screensize);
+     close(fbfd);
+     return 0;
+ }
